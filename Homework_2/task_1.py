5febdd1e8c9c4a54f001a3f173a28b58b4de3c42
andre <andreeff.vladimir@yandex.ru>
2024-01-21 19:03:40 +0300

    homework_2

---
+++ Homework_2/task_1.py
@@ -0,0 +1,10 @@
+def multiplication_table(n):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            product = i * j
+            print(f"{i} * {j} = {product}")
+        print("-------------")
+
+
+n = int(input('Enter n: '))
+multiplication_table(n)
